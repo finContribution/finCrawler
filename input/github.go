3507592c56e13a961549728ed818779ac7291d35
cucuridas <cucuridas@gamil.com>
2024-01-08 20:05:53 +0900

    REPACTOR - Simplifying GitHub API Call Structure and Adding Pagination Logic

--- input/github.go
+++ input/github.go
@@ -1,12 +1,12 @@
 package input
 
 import (
+	"fineC/util"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
-
-var url = "https://api.github.com/repos/"
 
 /*
 .env 파일에 추가한 token 정보를 가져옵니다 token 생성관련 내용은 링크 참조해주세요
@@ -14,44 +14,54 @@ https://docs.github.com/en/rest/authentication/authenticating-to-the-rest-api?ap
 */
 type GitHubClient struct {
 	Token string
-	Repo  *ProjectRepoInfo
+	Url   string
 }
 
 func NewGithubClient(token string, repo *ProjectRepoInfo) *GitHubClient {
+	var origin string = "https://api.github.com/repos"
 	return &GitHubClient{
-		token,
-		repo,
+		Token: token,
+		Url:   origin + "/" + repo.Owner + "/" + repo.Name + "/" + repo.ParseType,
 	}
+}
+
+func (c *GitHubClient) check(res *http.Response) bool {
+	links := res.Header.Get("Link")
+	return links != ""
 }
 
 /*
 해당 함수를 실행시킴으로 써 issue, pull request에 등록되어진 데이터를 조회합니다
 입력 받는 함수로서 프로젝트의 owner와 repo 이름, 그리고 수집하고자 하는 데이터의 타입을 입력받습니다(issue,pr)
 */
-func (c *GitHubClient) CallApi() []byte {
-
-	url := fmt.Sprintf(url+"%s/%s/%s", c.Repo.Name, c.Repo.Owner, c.Repo.ParseType)
-	req, err := http.NewRequest("GET", url, nil)
+func (c *GitHubClient) CallAPI(page int, ch chan<- []byte) {
+	url := c.Url + fmt.Sprintf("?page=%d", page)
+	resp, err := http.Get(url)
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Response was wrong, status code is %d", resp.StatusCode)
+	}
 	if err != nil {
-		fmt.Println("Error creating request:", err)
+		panic(err)
 	}
+	defer resp.Body.Close()
 
-	req.Header.Set("Authorization", "token "+c.Token)
-
-	response, err := http.DefaultClient.Do(req)
+	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error fetching data:", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		fmt.Println("Error: Non-OK HTTP status:", response.Status)
+		panic(err)
 	}
 
-	data, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-	}
+	ch <- data
+}
 
-	return data
+func (c GitHubClient) Crawler() *chan []byte {
+	ch := make(chan []byte)
+	for i := 1; i < util.APICounter; i++ {
+		go c.CallAPI(i, ch)
+
+		issues := <-ch
+		if len(issues) == 0 {
+			break
+		}
+	}
+	return &ch
 }
