4d9efe89253b7363691e97333906b0f241a48295
Chungeun Choi <ce.choi@okestro.com>
2024-01-23 08:43:21 +0900

    FEAT - Runner 생성 함수 개발 해당 함수는 cli로 입력되는 구조체를 통해 runner 객체를 생성해주는 함수

--- runner/runner.go
+++ runner/runner.go
@@ -4,11 +4,38 @@ import (
 	"fineC/input"
 	"fineC/output"
 	"fineC/util"
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
 type Runner struct {
-	Input  input.Input
-	Output output.Output
+	Input    input.Input
+	Output   output.Output
+	URL      string
+	SaveHost string
+}
+
+func NewRunner(inputFlag, outputFlag, url string) *Runner {
+	var inputObject input.Input
+	var outputObject output.Output
+
+	switch inputFlag {
+	case "github":
+		inputObject = &input.GitHubClient{Token: util.NewToken("../.env"), Url: url}
+
+	}
+	switch outputFlag {
+	case "elasticsearch":
+		client, _ := elasticsearch.NewDefaultClient()
+		outputObject = &output.ElasticsearchOutput{
+			Client:    client,
+			IndexName: "test-index",
+		}
+	}
+
+	return &Runner{
+		Input:  inputObject,
+		Output: outputObject,
+	}
 }
 
 func (r *Runner) Run() {
